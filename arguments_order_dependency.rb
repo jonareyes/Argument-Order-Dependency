a028d818f075a00903819c061c1dee8e82e371b1
codeamac09 <codeamac09@gmail.com>
2015-12-18 17:20:33 -0600

    Ejercicio para aprender a utilizar y detectar la forma keywword arguments

---
+++ arguments_order_dependency.rb
@@ -0,0 +1,39 @@
+class ContactInformation
+
+  def initialize(nombre:, telefono:, celular:, email:, referencia:, colonia:, municipio:, ciudad:, estado:, codigo_postal:, calle:, num_ext:, num_int:, pais: "Mexico", mayor_de_edad: false)
+    @nombre = nombre
+    @telefono = telefono
+    @celular = celular
+    @email = email
+    @referencia = referencia
+    @colonia = colonia
+    @municipio = municipio
+    @ciudad = ciudad
+    @estado = estado
+    @codigo_postal = codigo_postal
+    @calle = calle
+    @num_ext = num_ext
+    @num_int = num_int
+    @pais = pais
+    @mayor_de_edad = mayor_de_edad
+  end
+
+  def to_s
+    "Nombre: #{@nombre} #{@mayor_de_edad}, telefono: #{@telefono}, celular: #{@celular}, email: #{@email} \n Dirección: #{@calle}, #{@colonia}, #{@municipio}, #{@ciudad}, #{@estado}, #{@codigo_postal}, #{@pais} \n Referencia: #{@referencia}"
+  end
+
+  def numero
+    "#{@num_ext}#{"-#{@num_int}" if @num_int}"
+  end
+
+  def mayor_de_edad
+    unless @mayor_de_edad
+      "*"
+    end
+  end
+end
+puts ContactInformation.new(nombre: "Juan Perez Jr", telefono: "3891089238", celular: "04455832189491", email: "juanito@gmail.com", referencia: "entre calles x, z", municipio: "Cuauhtémoc", calle: "Benito Juárez", ciudad: "México", estado: "Distrito Federal", codigo_postal: "07500", colonia: "Chapultepec", num_ext: "500", num_int: "B")
+
+puts ContactInformation.new(nombre: "Juan Perez", telefono: "3891089238", celular: "04455832189491", email: "juanito@gmail.com", referencia: "entre calles x, z", municipio: "Cuauhtémoc", calle: "Benito Juárez",  ciudad: "México", estado: "Distrito Federal", codigo_postal: "07500", colonia: "Chapultepec", num_ext: "500", num_int: nil, mayor_de_edad: true)
+
+
